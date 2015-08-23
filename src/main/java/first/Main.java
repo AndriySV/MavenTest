0275351cc453f16d40497c1f88d9122af1ec0666
AndriySV <svyryd.andriy@gmail.com>
2015-08-23 18:11:01 +0300

    Initial commit

---
+++ src/main/java/first/Main.java
@@ -0,0 +1,31 @@
+package first;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>" + "My first servlet with Idea and Breakpoint !" + "</h1>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+        <h2>Hello World!</h2>
+
+        <h2> It's your Dog !</h2>
+
+        <h3>  Smile</h3>
+
+    </body>
+</html>
